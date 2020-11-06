9cb42c8881216f420120a545e9e28bd99c3e829b
uponup <1030360567@qq.com>
2020-11-06 16:21:37 +0800

    fix

--- MessageBroker/Classes/Utils/StatusQueue.swift
+++ MessageBroker/Classes/Utils/StatusQueue.swift
@@ -33,7 +33,7 @@ public class StatusQueue {
         guard let t = timer else {  return }
 
         t.fireDate = Date.distantFuture
-        RunLoop.current.add(t, forMode: .default)
+        RunLoop.current.add(t, forMode: .defaultRunLoopMode)
     }
     
     func updateUserStatus(imAccount account: String, status type: String = "online") {
