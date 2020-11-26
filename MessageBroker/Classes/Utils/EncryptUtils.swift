e3b8bc4bac9ef361221b01f1f67c7b586a19b17b
uponup <1030360567@qq.com>
2020-11-26 15:31:14 +0800

    fix ios 10

--- MessageBroker/Classes/Utils/EncryptUtils.swift
+++ MessageBroker/Classes/Utils/EncryptUtils.swift
@@ -10,38 +10,44 @@ import Foundation
 struct EncryptUtils {
     static func encrypt(_ data: String) -> String? {
         let ivKey = generateIvKey(len: data.bytes.count)
+        print("加密的ivKey:", ivKey)
         let cipherBytes = data.bytes.enumerated().map { (offset, element) -> UInt8 in
             let chr1 = ivKey[offset].value
-            let sum = UInt8((UInt16(chr1) + UInt16(element))%256)
-            return sum
+            let sum = UInt16(chr1)%256 + UInt16(element)
+            print("chr1: \(chr1)    chr2: \(element)    sum: \(sum)")
+            return UInt8(sum%256)
         }
-        let cipherText = Data(cipherBytes).base64EncodedString()
-        return cipherText
+
+        return Data(cipherBytes).base64EncodedString()
     }
 
     static func decrypt(_ cipherText: String) -> String? {
         let cipher = cipherText.removeWhitespaceAndNewLine()
-        guard let decodeData = NSData(base64Encoded: cipher, options: NSData.Base64DecodingOptions.init(rawValue: 0)) else {
+        guard let decodeData = Data(base64Encoded: cipher) else {
             print("解析失败")
             return nil
         }
+        
         let contentBytes = [UInt8](decodeData)
         
         let ivKey = generateIvKey(len: contentBytes.count)
-        let originText = contentBytes.enumerated().map{ (offset, element) -> UInt8 in
+        print("解密的ivKey：\(ivKey)")
+        let originBytes = contentBytes.enumerated().map{ (offset, element) -> UInt8 in
             let chr1 = UInt16(element)
             let chr2 = UInt16(ivKey[offset].value)
 
             if chr1 < chr2 {
                 let originChr = chr1 + 256 - chr2
+                print("chr1: \(chr1)    chr2: \(chr2)    origin: \(originChr)")
                 return UInt8(originChr)
             }else {
                 let originChr = chr1 - chr2
+                    
+                print("chr1: \(chr1)    chr2: \(chr2)    origin: \(originChr)")
                 return UInt8(originChr)
             }
         }
-        
-        return String(bytes: originText, encoding: .utf8)
+        return String(bytes: originBytes, encoding: .utf8)
     }
 
     private static func generateIvKey(len: Int) -> String {
