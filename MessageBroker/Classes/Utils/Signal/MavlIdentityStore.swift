34b2f82e9ac99aecccdc04adbe8e67e74718e6a3
uponup <1030360567@qq.com>
2021-01-30 11:37:27 +0800

    fix

--- MessageBroker/Classes/Utils/Signal/MavlIdentityStore.swift
+++ MessageBroker/Classes/Utils/Signal/MavlIdentityStore.swift
@@ -12,7 +12,7 @@ class MavlIdentityStore: IdentityKeyStore {
     typealias Address = MavlAddress
     
     private var identityKey: Data!
-    private var identities = [MavlAddress: Data]()
+    private var identities = [Address: Data]()
     private let MavlIdentityStoreKey = "MavlIdentityStoreKey"
     
     required init(with keyPair: Data) {
@@ -26,16 +26,16 @@ class MavlIdentityStore: IdentityKeyStore {
         return identityKey
     }
     
-    func identity(for address: MavlAddress) throws -> Data? {
-        guard let ids = MavlKeyStore.store(forKey: MavlIdentityStoreKey, dictKeyType: MavlAddress.self) else {
-            throw SignalError(.storageError, "No identity key for address \(address)")
+    func identity(for address: Address) throws -> Data? {
+        guard let ids = MavlKeyStore.store(forKey: MavlIdentityStoreKey, dictKeyType: Address.self) else {
+            return nil
         }
         identities = ids
         return identities[address]
     }
     
     // 存储已经被信任的id
-    func store(identity: Data?, for address: MavlAddress) throws {
+    func store(identity: Data?, for address: Address) throws {
         identities[address] = identity
         MavlKeyStore.setStore(store: identities, forKey: MavlIdentityStoreKey)
     }
