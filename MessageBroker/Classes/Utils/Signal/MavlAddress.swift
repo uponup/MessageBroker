1ebe0a25b900fa3937ac5855e2c8c743dc7620c9
uponup <1030360567@qq.com>
2021-01-30 19:50:40 +0800

    fix持久化导致signal加解密失败的问题

--- MessageBroker/Classes/Utils/Signal/MavlAddress.swift
+++ MessageBroker/Classes/Utils/Signal/MavlAddress.swift
@@ -6,47 +6,23 @@
 //
 
 import Foundation
-/**
-    1、数据持久化采用了NSKeyedArchive来处理，要保存的数据中可能含有MavlAddress类型的key，所以MavlAddress需要遵循NSCoding协议。所以选择MavlAddress为class数据结构。
-    2、NSCoding协议为NSObject的协议，普通的元类是不能遵守这个协议，所以让MavlAddress继承自NSObject。
-    3、此外，NSObject还自动实现了Hashable和Equatable协议，满足了将MavlAddress作为字典中的key的数据类型。
- */
 
-class MavlAddress: NSObject, NSCoding, NSCopying {
+struct MavlAddress {
     let identifier: String
-    
-    init(identifier: String) {
-        self.identifier = identifier
-    }
-    
-    func encode(with coder: NSCoder) {
-        coder.encode(identifier, forKey: "identifier")
-    }
-    
-    required convenience init?(coder: NSCoder) {
-        let identifier = (coder.decodeObject(forKey: "identifier") as? String) ?? ""
-        self.init(identifier: identifier)
-    }
-    
-    func copy(with zone: NSZone? = nil) -> Any {
-        return MavlAddress(identifier: self.identifier)
+}
+
+extension MavlAddress: Equatable {
+    static func ==(lhs: MavlAddress, rhs: MavlAddress) -> Bool {
+        return lhs.identifier == rhs.identifier
     }
 }
 
-//struct MavlAddress {
-//    let identifier: String
-//}
-//
-//extension MavlAddress: Equatable {
-//    static func ==(lhs: MavlAddress, rhs: MavlAddress) -> Bool {
-//        return lhs.identifier == rhs.identifier
-//    }
-//}
-//
-//extension MavlAddress: Hashable {}
-//
-//extension MavlAddress: CustomStringConvertible {
-//    var description: String {
-//        return "MavlAddress: \(identifier)"
-//    }
-//}
+extension MavlAddress: Hashable {}
+extension MavlAddress: Codable {}
+
+extension MavlAddress: CustomStringConvertible {
+    var description: String {
+        return "MavlAddress: \(identifier)"
+    }
+}
+
--- MessageBroker/Classes/Utils/Signal/MavlKeyStore.swift
+++ MessageBroker/Classes/Utils/Signal/MavlKeyStore.swift
@@ -30,15 +30,35 @@ class MavlKeyStore: KeyStore {
 
 // MARK: - 持久化
 extension MavlKeyStore {
-    class func store<T>(forKey key:String, dictKeyType: T.Type) -> [T: Data]? {
+    class func store<T: Codable>(forKey key:String, dictKeyType: T.Type) -> [T: Data]? {
         guard let data = UserDefaults.object(forKey: key) as? Data,
-              let sessionsDict = data.unarchiveDataToDict(keyType: dictKeyType) else {
+              let dict = data.unarchiveDataToDict(keyType: Data.self) else {
             return nil
         }
+        let sessionsDict = Dictionary(uniqueKeysWithValues: dict.map({ (sessionKey, value) -> (T, Data) in
+            let obj = try!JSONDecoder().decode(T.self, from: sessionKey)
+            return (obj, value)
+        }))
         return sessionsDict
     }
     
-    class func setStore<T>(store:[T: Data], forKey key: String) {
+    class func store<T: UnsignedInteger>(forKey key:String, dictKeyType: T.Type) -> [T: Data]? {
+        guard let data = UserDefaults.object(forKey: key) as? Data,
+              let dict = data.unarchiveDataToDict(keyType: T.self) else {
+            return nil
+        }
+        return dict
+    }
+    
+    class func setStore<T: UnsignedInteger>(store:[T: Data], forKey key: String) {
         UserDefaults.set(store.archivedData, forKey: key)
+    }
+    
+    class func setStore<T: Codable>(store:[T: Data], forKey key: String) {
+        let dict = Dictionary(uniqueKeysWithValues: store.map({ (sessionKey, value) -> (Data, Data) in
+            let data = try!JSONEncoder().encode(sessionKey)
+            return (data, value)
+        }))
+        UserDefaults.set(dict.archivedData, forKey: key)
     }
 }
